fe8990164fe55f02961a49df546fabf116965961
jaz <jsmith6112@gmail.com>
2020-06-01 16:21:48 -0400

    Update to case Assignment throught #6

--- Case Assignment - Employee SQL/Notepad files/Employee Analysis query.sql
+++ Case Assignment - Employee SQL/Notepad files/Employee Analysis query.sql
@@ -42,16 +42,44 @@ INNER JOIN employees e ON
 
 ---4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
+SELECT 
+	 	e.emp_no AS "Employee Number"
+	   ,e.last_name AS "Last Name"
+	   ,e.first_name AS "First Name"
+	   ,d.dept_name as "Department Name"
+FROM employees e
 
-
-
-
+INNER JOIN "Dept_emp" de  ON
+	   de.emp_no = e.emp_no
+INNER JOIN departments d ON
+		d.dept_no = de.dept_no;
 
 
 ---5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
+SELECT
+	e.last_name AS "Last Name"
+	,e.first_name AS "First Name"
+	,e.sex AS "Gender"
+FROM 
+	employees e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
+
+
 ---6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+
+
+
+
+
+
 ---7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+
+
+
+
 
 ---8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
