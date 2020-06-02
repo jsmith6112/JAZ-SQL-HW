b1fdd5570222d9b79801cb256024746b661fc063
jaz <jsmith6112@gmail.com>
2020-06-02 12:07:42 -0400

    Final Version complete

--- Case Assignment - Employee SQL/Notepad files/Employee Analysis query.sql
+++ Case Assignment - Employee SQL/Notepad files/Employee Analysis query.sql
@@ -1,4 +1,3 @@
-
 --- DATA ANALYSIS
 --- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
@@ -63,23 +62,51 @@ SELECT
 	,e.sex AS "Gender"
 FROM 
 	employees e
-WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
+WHERE e.first_name = 'Hercules' AND upper(e.last_name) LIKE 'B%';
 
 
 ---6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+SELECT
+	 e.emp_no AS "Employee Number"
+	 ,e.last_name AS "Last Name"
+	 ,e.first_name AS "First Name"
+	 ,d.dept_name AS "Department"
 
+FROM employees e
 
+	INNER JOIN "Dept_emp" de ON
+	 	de.emp_no = e.emp_no
 
-
+	INNER JOIN departments d ON
+		d.dept_no = de.dept_no 
+		AND d.dept_name = 'Sales';
 
 
 ---7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+SELECT
+	 e.emp_no AS "Employee Number"
+	 ,e.last_name AS "Last Name"
+	 ,e.first_name AS "First Name"
+	 ,d.dept_name AS "Department"
 
+FROM employees e
 
+	INNER JOIN "Dept_emp" de ON
+	 	de.emp_no = e.emp_no
 
-
-
+	INNER JOIN departments d ON
+		d.dept_no = de.dept_no 
+		AND d.dept_name IN ('Sales','Development');
 
 ---8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+Select last_name AS "Last Name"
+	,COUNT(last_name) AS "Count Frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY count(last_name) DESC;
+
+
+
